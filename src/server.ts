345d2863d82466a8d4de4a989a355b20f34c3a7f
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-09-09 00:01:12 +0200

    post an applicant

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,10 @@ app.use(express.json())
 
 const port = 5000
 
+const getApplicants=db.prepare(`
+SELECT * FROM applicants;
+`)
+
 const getSingleApplicant = db.prepare(`
 SELECT * FROM applicants WHERE id=@id;
 `)
@@ -26,8 +30,17 @@ const getSingleInterviewer = db.prepare(`
 SELECT * FROM interviewers WHERE id=@id;
 `)
 
+const postNewApplicant=db.prepare(`
+INSERT INTO applicants(name, age, email) VALUES(@name, @age, @email)
+`)
+
 app.get('/', (req, res) => {
     res.send("hello")
+})
+
+app.get('/applicants', (req, res)=>{
+    const applicants=getApplicants.all()
+    res.send(applicants)
 })
 
 app.get('/applicants/:id', (req, res) => {
@@ -57,6 +70,29 @@ app.get('/interviewers/:id', (req, res) => {
     else {
         res.status(404).send({ error: "Interviewer not found!" })
     }
+})
+
+app.post('/applicants', (req, res)=>{
+const errors:string[]=[]
+
+if(typeof req.body.name !=="string"){
+    errors.push("Please enter a valid name")
+}
+if(typeof req.body.age !== "number"){
+    errors.push("Please enter a valid age")
+}
+if(typeof req.body.email !=="string"){
+    errors.push("Please enter a valid email")
+}
+
+if(errors.length===0){
+    const applicantInfo=postNewApplicant.run(req.body)
+    const applicant=getSingleApplicant.get({id: applicantInfo.lastInsertRowid})
+    res.send(applicant)
+}
+else{
+    res.status(400).send({errors: errors})
+}
 })
 
 
